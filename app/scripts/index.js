87796c5514100b00c218e0135500d9297eb1d3a1
Hannah Smith <hannahsmith@Hannahs-MacBook-Pro.local>
2016-03-03 23:06:41 -0500

    styled players and wrote attack button in js

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+var $ = require ('jquery');
+var _ = require ('underscore');
+
+function Player(options){
+ options = options || {};
+ this.health = options.health || 100;
+}
+
+Player.prototype.takeDamage = function(){
+ this.health = this.health - 10;
+}
+
+
+var puppy = new Player({health: 200});
+
+var kitten = new Player({health: 100});
+
+$('.attack-button').on('click', function(){
+ puppy.takeDamage();
+ kitten.takeDamage();
+ $('.kittens .health').text(kitten.health);
+ console.log(kitten.health);
+ $('.puppies .health').text(puppy.health);
+ console.log(puppy.health);
+});
